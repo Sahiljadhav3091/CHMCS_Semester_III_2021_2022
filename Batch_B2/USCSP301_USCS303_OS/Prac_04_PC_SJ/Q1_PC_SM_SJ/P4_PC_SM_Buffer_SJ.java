bd8e3b10420a607a0be365b68a5609480bbf99de
Sahiljadhav3091 <87857562+Sahiljadhav3091@users.noreply.github.com>
2021-09-02 16:20:33 +0530

    Add files via upload

---
+++ Batch_B2/USCSP301_USCS303_OS/Prac_04_PC_SJ/Q1_PC_SM_SJ/P4_PC_SM_Buffer_SJ.java
@@ -0,0 +1,13 @@
+//Name: Sahil Jadhav
+//Batch No:B2
+//PRN:2020016400783091
+//Date:06-08-2021
+
+public interface P4_PC_SM_Buffer_SJ
+{
+	//Procedures call this method
+	public void insert(String item);
+
+	//Consumers call this method
+  	public String remove();
+}//interface ends
